bdf74c739cf040dd2206792ae8bb8253c9203cdb
Will Franklin <will.franklin@guardian.co.uk>
2016-01-06 11:43:58 +0000

    Standard fonts, move some styles

---
+++ src/js/lib/chart.js
@@ -0,0 +1,2 @@
+export default function chart(el, stats) {
+}
--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,20 @@ import doT from 'olado/doT'
 import share from './lib/share'
 import sheetURL from './lib/sheetURL'
 import groupBy from './lib/groupBy'
+import chart from './lib/chart'
 
 import mainHTML from './text/main.html!text'
 
 const docID = '1_z0hqi6kD1o9qmSl7xwvP__bIkaC060uIB2cv8eNS2Y';
 const sheetID = '1D12VO4GvbCOHoD6_TNVcmtR2EU0JDxIfzb5ZuO6m-5k';
 
-const headers = ['group', 'year', 'month', 'week'];
+const headers = ['series', 'year', 'month', 'week'];
 
 var templateFn = doT.template(mainHTML);
+
+function $$(el, s) {
+    return [].slice.apply(el.querySelectorAll(s));
+}
 
 function parseNumber(s) {
     return parseInt(s.replace(/,/g, ''));
@@ -31,28 +36,26 @@ function app(el, config, doc, sheet) {
     types.forEach(type => {
         var typeStats = sheet.sheets.data.map(row => {
             return {
-                'group': parseNumber(row.group),
+                'series': parseNumber(row.series),
                 'year': parseNumber(row.year),
                 'month': row.month,
                 'week': parseNumber(row.week),
                 'value': parseNumber(row[type])
             };
         });
-        stats[type] = groupBy(typeStats, 'group');
+        stats[type] = groupBy(typeStats, 'series');
     });
 
-    console.log(stats);
-
     doc.sections.forEach(section => {
-        section.stats = stats[section.type];
-        section.statsLatest = last(last(section.stats).value.filter(stat => !isNaN(stat.value)));
-
-        section.statText = section.stat.replace('{x}', section.statsLatest.value);
+        var latestStat = last(last(stats[section.type]).value.filter(stat => !isNaN(stat.value)));
+        section.stat = section.stat.replace('{x}', latestStat.value.toLocaleString());
     });
 
     el.innerHTML = templateFn(doc);
 
-    [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
+    $$('.js-chart').forEach(chartEl => chart(chartEl, stats[type]));
+
+    $$('.interactive-share').forEach(shareEl => {
         var network = shareEl.getAttribute('data-network');
         shareEl.addEventListener('click',() => shareFn(network));
     });
