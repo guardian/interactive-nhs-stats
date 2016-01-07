c8473d7f7252dafac5c0fd5ff1ce55be2d51e7d3
Will Franklin <will.franklin@guardian.co.uk>
2016-01-07 12:34:00 +0000

    Remove updating salary, it would take ~30 mins change. Some styling

--- src/js/components/salary.js
+++ src/js/components/salary.js
@@ -1,18 +1,7 @@
 const start = +new Date(2016, 0, 1);
 const secsPerYear = 366 * 24 * 60 * 60;
 
-var salaries = [];
-
-function update() {
-    var delta = (+new Date() - start) / 1000;
-    salaries.forEach(s => {
-        s.el.textContent = Math.floor(s.salary * delta).toLocaleString();
-    });
-}
-
-setInterval(update, 10000);
-
 export default function createSalary(el, salary) {
-    salaries.push({el, 'salary': salary / secsPerYear});
-    update();
+    var delta = (+new Date() - start) / 1000;
+    el.textContent = Math.floor(salary / secsPerYear * delta).toLocaleString();
 }
